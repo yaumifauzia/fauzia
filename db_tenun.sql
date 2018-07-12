a61d92a0c0a696214d76298a1a17bc61947a25ad
yaumifauzia <yaumifauiza5@gmail.com>
2018-07-12 17:35:34 +0700

    push db_tenun

---
+++ db_tenun.sql
@@ -0,0 +1,357 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 12, 2018 at 12:33 PM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_tenun`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banner`
+--
+
+CREATE TABLE `banner` (
+  `banner_id` int(10) NOT NULL,
+  `banner` varchar(100) NOT NULL,
+  `gambar` varchar(150) NOT NULL,
+  `link` varchar(500) NOT NULL,
+  `status` enum('on','off') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `banner`
+--
+
+INSERT INTO `banner` (`banner_id`, `banner`, `gambar`, `link`, `status`) VALUES
+(3, 'Songket', 'lima.jpg', '', 'off'),
+(4, 'Silungkang', 'tenun.jpg', '', 'on'),
+(5, 'Songket', 'empat.jpg', '', 'on');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `barang_id` int(10) NOT NULL,
+  `kategori_id` int(10) NOT NULL,
+  `nama_barang` varchar(250) NOT NULL,
+  `spesifikasi` text NOT NULL,
+  `gambar` varchar(300) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `stok` tinyint(1) NOT NULL,
+  `status` enum('on','off') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`barang_id`, `kategori_id`, `nama_barang`, `spesifikasi`, `gambar`, `harga`, `stok`, `status`) VALUES
+(16, 1, 'baju', '<p>eegrgtrgtrgregertgr</p>', 'dua.jpg', 2500000, 11, 'on'),
+(17, 4, 'songket setelan', '', 'hitam.jpg', 250000, 0, 'on');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `kategori_id` int(10) NOT NULL,
+  `kategori` varchar(150) NOT NULL,
+  `status` enum('on','off') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`kategori_id`, `kategori`, `status`) VALUES
+(1, 'Silungkang', 'on'),
+(2, 'Tenun Balopak', 'on'),
+(3, 'Bahan Pakian', 'on'),
+(4, 'Songket', 'on');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `konfirmasi_pembayaran`
+--
+
+CREATE TABLE `konfirmasi_pembayaran` (
+  `konfirmasi_id` int(10) NOT NULL,
+  `pesanan_id` int(10) NOT NULL,
+  `nomor_rekening` varchar(20) NOT NULL,
+  `nama_account` varchar(150) NOT NULL,
+  `tanggal_transfer` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `konfirmasi_pembayaran`
+--
+
+INSERT INTO `konfirmasi_pembayaran` (`konfirmasi_id`, `pesanan_id`, `nomor_rekening`, `nama_account`, `tanggal_transfer`) VALUES
+(1, 2, '0008888', 'Jeong', '2016-06-19'),
+(2, 3, '0008888', 'Lee', '2016-06-19'),
+(3, 4, '0008888', 'Jeong', '2016-06-19'),
+(4, 5, '122456457568678', 'dela', '2018-07-11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kota`
+--
+
+CREATE TABLE `kota` (
+  `kota_id` int(10) NOT NULL,
+  `kota` varchar(150) NOT NULL,
+  `tarif` int(10) NOT NULL,
+  `status` enum('on','off') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kota`
+--
+
+INSERT INTO `kota` (`kota_id`, `kota`, `tarif`, `status`) VALUES
+(1, 'Jakarta', 6000, 'on'),
+(2, 'Bandung', 8000, 'on'),
+(3, 'Butiinggi', 5000, 'on'),
+(4, 'Padang', 9000, 'on');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `pesanan_id` int(10) NOT NULL,
+  `kota_id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `nama_penerima` varchar(150) NOT NULL,
+  `nomor_telepon` varchar(15) NOT NULL,
+  `alamat` varchar(300) NOT NULL,
+  `tanggal_pemesanan` datetime NOT NULL,
+  `status` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pesanan`
+--
+
+INSERT INTO `pesanan` (`pesanan_id`, `kota_id`, `user_id`, `nama_penerima`, `nomor_telepon`, `alamat`, `tanggal_pemesanan`, `status`) VALUES
+(2, 2, 2, 'Jeong', '00000', 'Jl. XXXXX', '2016-10-08 06:11:24', 1),
+(3, 3, 6, 'Lee', '0000', 'Jl. aaaa', '2016-10-08 10:48:45', 2),
+(4, 1, 6, 'Lee', '0000', 'Jl. AAA', '2016-10-08 12:01:55', 2),
+(5, 3, 7, 'dela', '12345678', 'payakumbuh', '2018-07-11 12:48:26', 1),
+(6, 3, 7, 'dela', '12345678', 'bukittinggi', '2018-07-11 13:11:43', 0),
+(7, 3, 7, 'dela', '12345678', 'payakumbuh', '2018-07-11 13:18:46', 0),
+(8, 1, 7, 'dela', '12345678', 'payakumbuh', '2018-07-11 13:33:57', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan_detail`
+--
+
+CREATE TABLE `pesanan_detail` (
+  `pesanan_id` int(10) NOT NULL,
+  `barang_id` int(10) NOT NULL,
+  `quantity` tinyint(2) NOT NULL,
+  `harga` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pesanan_detail`
+--
+
+INSERT INTO `pesanan_detail` (`pesanan_id`, `barang_id`, `quantity`, `harga`) VALUES
+(5, 16, 1, 2500000),
+(6, 16, 1, 2500000),
+(7, 16, 1, 2500000),
+(8, 16, 1, 2500000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(10) NOT NULL,
+  `level` varchar(30) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `email` varchar(160) NOT NULL,
+  `alamat` varchar(400) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `status` enum('on','off') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `level`, `nama`, `email`, `alamat`, `phone`, `password`, `status`) VALUES
+(2, 'superadmin', 'admin', 'fauzia001@gmail.com', 'balai panjang', '08889999', '1b3231655cebb7a1f783eddf27d254ca', 'on'),
+(6, 'customer', 'customer', 'customer1@aaa.com', 'jl.Customer Weshop', '088888', '5f4dcc3b5aa765d61d8327deb882cf99', 'on'),
+(7, 'customer', 'dela', 'dela01', 'payakumbuh', '123456', '827ccb0eea8a706c4c34a16891f84e7b', 'on');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `banner`
+--
+ALTER TABLE `banner`
+  ADD PRIMARY KEY (`banner_id`);
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`barang_id`),
+  ADD KEY `kategori_id` (`kategori_id`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`kategori_id`);
+
+--
+-- Indexes for table `konfirmasi_pembayaran`
+--
+ALTER TABLE `konfirmasi_pembayaran`
+  ADD PRIMARY KEY (`konfirmasi_id`),
+  ADD KEY `pesanan_id` (`pesanan_id`);
+
+--
+-- Indexes for table `kota`
+--
+ALTER TABLE `kota`
+  ADD PRIMARY KEY (`kota_id`);
+
+--
+-- Indexes for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`pesanan_id`),
+  ADD KEY `kota_id` (`kota_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `pesanan_detail`
+--
+ALTER TABLE `pesanan_detail`
+  ADD KEY `pesanan_id` (`pesanan_id`),
+  ADD KEY `barang_id` (`barang_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `banner`
+--
+ALTER TABLE `banner`
+  MODIFY `banner_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `barang_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `kategori_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `konfirmasi_pembayaran`
+--
+ALTER TABLE `konfirmasi_pembayaran`
+  MODIFY `konfirmasi_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `kota`
+--
+ALTER TABLE `kota`
+  MODIFY `kota_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `pesanan_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `barang`
+--
+ALTER TABLE `barang`
+  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`kategori_id`) REFERENCES `kategori` (`kategori_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `konfirmasi_pembayaran`
+--
+ALTER TABLE `konfirmasi_pembayaran`
+  ADD CONSTRAINT `konfirmasi_pembayaran_ibfk_1` FOREIGN KEY (`pesanan_id`) REFERENCES `pesanan` (`pesanan_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD CONSTRAINT `pesanan_ibfk_1` FOREIGN KEY (`kota_id`) REFERENCES `kota` (`kota_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pesanan_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pesanan_detail`
+--
+ALTER TABLE `pesanan_detail`
+  ADD CONSTRAINT `pesanan_detail_ibfk_1` FOREIGN KEY (`pesanan_id`) REFERENCES `pesanan` (`pesanan_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pesanan_detail_ibfk_2` FOREIGN KEY (`barang_id`) REFERENCES `barang` (`barang_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
